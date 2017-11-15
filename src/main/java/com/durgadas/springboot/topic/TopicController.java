8885f0662bb31683c2da8cecee0cbbba29796908
imDurgadas <durgadasthegenius@gmail.com>
2017-11-15 15:46:27 +0530

    Add CrossOrigin

--- src/main/java/com/durgadas/springboot/topic/TopicController.java
+++ src/main/java/com/durgadas/springboot/topic/TopicController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/topics")
 @Api(value="Topics")
+@CrossOrigin
 public class TopicController {
 	
 	@Autowired
